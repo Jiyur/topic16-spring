f0c2c7d65189dff60ac467e2dd33603fe69d1d0a
Jiyur <53509652+Jiyur@users.noreply.github.com>
2022-01-24 09:54:24 +0700

    implement homepage

--- src/main/java/abc/com/vn/commerce/controller/CustomerController.java
+++ src/main/java/abc/com/vn/commerce/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CustomerController {
@@ -34,7 +36,7 @@ public class CustomerController {
 //        }
 //        return "index";
 //    }
-    @GetMapping(value = {"/","/home"})
+    @RequestMapping(value = {"/","/home"},method = RequestMethod.GET)
     public String home(){
         return "index";
     }
