21a71a1f41b93aebd44674124b95a63b816e49ce
Jiyur <53509652+Jiyur@users.noreply.github.com>
2022-01-24 14:27:34 +0700

    implement admin page and refactor project structure

--- src/main/java/abc/com/vn/commerce/controller/customer_site/CustomerController.java
+++ src/main/java/abc/com/vn/commerce/controller/customer_site/CustomerController.java
@@ -1,11 +1,9 @@
-package abc.com.vn.commerce.controller;
+package abc.com.vn.commerce.controller.customer_site;
 
-import abc.com.vn.commerce.model.Customer;
 import abc.com.vn.commerce.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,6 +13,7 @@ public class CustomerController {
 
     @Autowired
     CustomerService customerDAO;
+
 
 //    @GetMapping(value = "/save")
 //    public String saveCustomer(Model model){
@@ -36,6 +35,6 @@ public class CustomerController {
 //    }
     @GetMapping(value = {"/","/home"})
     public String home(){
-        return "index";
+        return "/customer/index";
     }
 }
