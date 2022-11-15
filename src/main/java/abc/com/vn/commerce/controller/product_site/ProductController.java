87fb7fa4070c185479ab8085a13e11a508328dde
@vantinh101 <vantinh101@gmail.com>
2022-11-15 23:01:18 +0700

    add get productById controller

---
+++ src/main/java/abc/com/vn/commerce/controller/product_site/ProductController.java
@@ -0,0 +1,26 @@
+package abc.com.vn.commerce.controller.product_site;
+
+import abc.com.vn.commerce.model.Product;
+import abc.com.vn.commerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProductController {
+
+  @Autowired
+  ProductService productService;
+
+  @GetMapping("/product/{id}")
+  public String getAllById(Model model, @PathVariable("id") Integer id) {
+    Product product = productService.getProductById(id);
+    if (product == null) {
+      return "/home";
+    }
+    model.addAttribute("product", product);
+    return "/product/" + id;
+  }
+}
